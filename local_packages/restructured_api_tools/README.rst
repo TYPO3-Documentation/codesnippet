388edc417981a20ade085ffa5d7ae6175104d652
lina.wolf <lwolf@w-commerce.de>
2022-04-24 15:13:32 +0200

    [TASK] Move Files to local_packages releases: main

---
+++ local_packages/restructured_api_tools/README.rst
@@ -0,0 +1,8 @@
+
+=====
+Usage
+=====
+
+.. code-block:: shell
+
+    bin/typo3  restructured_api_tools:php_domain '\\TYPO3\\CMS\\Core\\Domain\\Access\\RecordAccessGrantedEvent' > output/RecordAccessGrantedEvent.rst.txt
---
+++ local_packages/restructured_api_tools/ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Restructured API tools',
+    'description' => 'This extension packages creates restructured files to document the API',
+    'category' => 'distribution',
+    'author' => 'Documentation Team',
+    'author_email' => 'documentation@typo3.org',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author_company' => '',
+    'version' => '12.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
