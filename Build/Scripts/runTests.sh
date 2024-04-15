d5963ee3b512c785a5bfecf6065be518804fab62
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-04-15 12:02:50 +0200

    Update PHP versions

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -166,7 +166,7 @@ while getopts ":s:p:t:e:xynhuv" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8.1|8.2|8.3)$ ]]; then
                 INVALID_OPTIONS+=("p ${OPTARG}")
             fi
             ;;
