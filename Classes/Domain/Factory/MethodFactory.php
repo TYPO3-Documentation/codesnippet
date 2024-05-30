8a91e18f4508ee635a59bf98446f8c5bc8ef1662
lina.wolf <lwolf@w-commerce.de>
2024-05-30 18:04:08 +0200

    [BUGFIX] Prevent return types from being overridden by parameter types

--- Classes/Domain/Factory/MethodFactory.php
+++ Classes/Domain/Factory/MethodFactory.php
@@ -36,12 +36,8 @@ class MethodFactory
     /**
      * Extract method from class
      *
-     * @param string $class Class name, e.g. "TYPO3\CMS\Core\Cache\Backend\FileBackend"
-     * @param string $method Method name, e.g. "freeze"
-     * @param bool $withCode Include the complete method as code example?
      * @param int $modifierSum sum of all modifiers (i.e. \ReflectionMethod::IS_PUBLIC + \ReflectionMethod::IS_PROTECTED)
-     * @param bool $allowInternal Include Internal methods?
-     * @param bool $allowDeprecated Include Deprecated methods?
+
      * @return string
      */
     public function getMethod(
@@ -53,14 +49,9 @@ class MethodFactory
         bool $includeConstructor,
     ): MethodMember|null {
         $methodReflection = $reflectionClass->getMethod($method);
-        $isInternal = is_string($methodReflection->getDocComment())
-            && str_contains($methodReflection->getDocComment(), '* @internal');
-        // For some reason $methodReflection->isInternal() is always false
+        $isInternal = $this->isInternal($methodReflection);
         if (
-            (!$allowInternal && $isInternal)
-            or (!$allowDeprecated && $methodReflection->isDeprecated())
-            or (($modifierSum & $methodReflection->getModifiers()) == 0)
-            or (!$includeConstructor && $method == '__construct')
+            $this->isMethodDisAllowed($allowInternal, $isInternal, $allowDeprecated, $methodReflection, $modifierSum, $includeConstructor, $method)
         ) {
             return null;
         }
@@ -73,63 +64,10 @@ class MethodFactory
         $startLineBody = $methodReflection->getStartLine();
         $endLineBody = $methodReflection->getEndLine();
 
-        $startLineSignature = max($startLineBody - 20, 0);
-        for ($lineNumber = $startLineSignature; $lineNumber <= $startLineBody; $lineNumber++) {
-            $splFileObject->seek($lineNumber);
-            if (str_contains($splFileObject->current(), sprintf('function %s', RstHelper::escapeRst($method)))) {
-                $startLineSignature = $lineNumber;
-            }
-        }
+        $startLineSignature = $this->findLineStart($startLineBody, $splFileObject, $method);
         $returnType = $methodReflection->getReturnType();
         $parameters = $methodReflection->getParameters();
-        $parameterResolved = [];
-        foreach ($parameters as $parameter) {
-            $paramName = $parameter->getName();
-            $returnType = '';
-            if ($parameter->getType() instanceof \ReflectionNamedType) {
-                $returnType = $parameter->getType()->getName();
-            } elseif ($parameter->getType() instanceof \ReflectionUnionType) {
-                $types = $parameter->getType()->getTypes();
-                $typeNameArray = [];
-                foreach ($types as $returnType) {
-                    $typeNameArray[] = $returnType->getName();
-                }
-                $returnType = implode('|', $typeNameArray);
-            } elseif ($parameter->getType() instanceof \ReflectionIntersectionType) {
-                $types = $parameter->getType()->getTypes();
-                $typeNameArray = [];
-                foreach ($types as $typeElement) {
-                    $typeNameArray[] = $typeElement->getName();
-                }
-                $returnType = implode('&', $typeNameArray);
-            }
-            // Check if the parameter allows null
-            $nullable = $parameter->allowsNull() ? '?' : '';
-            $optional = $parameter->isOptional();
-            $default = '';
-            if ($optional) {
-                try {
-                    $default = ArrayHelper::varExportArrayShort($parameter->getDefaultValue());
-                } catch (\ReflectionException) {
-                }
-            }
-            // Check if the parameter is passed by reference
-            $passedByReference = $parameter->isPassedByReference() ? '&' : '';
-
-            // Check if the parameter is variadic
-            $variadic = $parameter->isVariadic() ? '...' : '';
-
-            $parameterResolved[] = [
-                'name' =>  '$' . $paramName,
-                'type' => $returnType,
-                'optional' => $optional,
-                'default' => $default,
-                'description' => '',
-                'passedByReference' => $passedByReference,
-                'variadic' => $variadic,
-                'nullable' => $nullable,
-            ];
-        }
+        $parameterResolved = $this->resolveParameters($parameters);
         $docComment = $methodReflection->getDocComment();
         $comment = '';
         $returnComment = '';
@@ -151,34 +89,192 @@ class MethodFactory
                     $returnComment = str_replace('@return ', '', $returnCommentTagArray[0]->render());
                 }
                 $paramArray = $docBlock->getTagsByName('param');
-                if (is_array($paramArray) && count($paramArray) > 0) {
-                    foreach ($paramArray as $param) {
-                        $paramCommentExplode = explode(' ', $param->render(), 4);
-                        $paramName = null;
-                        $returnType = null;
-                        $description = '';
-                        if (count($paramCommentExplode) > 2) {
-                            $paramName = $paramCommentExplode[2];
-                            $returnType = $paramCommentExplode[1];
-                            $description = $paramCommentExplode[3] ?? '';
-                        }
-                        foreach ($parameterResolved as $key => $paramResolved) {
-                            if ($parameterResolved[$key]['name'] === $paramName) {
-                                // Type from method reflection is considered more accurate
-                                if (!$parameterResolved[$key]['type']) {
-                                    $parameterResolved[$key]['type'] = $returnType;
-                                }
-                                $parameterResolved[$key]['description'] = $description;
-                            }
-                        }
-                    }
-                }
+                $parameterResolved = $this->enrichParametersResolvedWithDocComment($paramArray, $parameterResolved);
             } catch (\Exception) {
                 // doccomment cannot be interpreted
                 // keep data from method reflection
             }
         }
 
+        list($parameterInSignature, $parameters) = $this->getParameters($parameterResolved);
+        $codeResult = [];
+        for ($lineNumber = $startLineSignature; $lineNumber < $endLineBody; $lineNumber++) {
+            $splFileObject->seek($lineNumber);
+            $codeResult[] = $splFileObject->current();
+        }
+        $code = implode('', $codeResult);
+
+        $typeNames = '';
+        if ($returnType instanceof \ReflectionUnionType or $returnType instanceof \ReflectionNamedType && $returnType->getName() != 'void') {
+            if ($returnType instanceof \ReflectionNamedType) {
+                $typeNames = $returnType->allowsNull()
+                    ? '?' . self::getFullQualifiedClassNameIfPossible($returnType->getName())
+                    : self::getFullQualifiedClassNameIfPossible($returnType->getName());
+            } elseif ($returnType instanceof \ReflectionUnionType) {
+                $types = $returnType->getTypes();
+                $typeNameArray = [];
+                foreach ($types as $returnType) {
+                    $typeNameArray[] = $returnType->allowsNull()
+                        ? '?' . self::getFullQualifiedClassNameIfPossible($returnType->getName())
+                        : self::getFullQualifiedClassNameIfPossible($returnType->getName());
+                }
+                $typeNames = implode('|', $typeNameArray);
+            }
+        }
+
+        $modifiers = [];
+        list($returnType, $returnComment) = $this->extractTypeFromReturnComment($typeNames, $returnComment);
+
+        // SplFileObject locks the file, so null it when no longer needed
+        $splFileObject = null;
+        //return $methodHead . $methodBody;
+        return new MethodMember(
+            $methodReflection->getName(),
+            $comment,
+            $modifiers,
+            $code,
+            $parameters,
+            $returnType,
+            str_replace("\n", '', $returnComment),
+            implode(', ', $parameterInSignature),
+        );
+    }
+
+    private function getFullQualifiedClassNameIfPossible(string $className): string
+    {
+        try {
+            $reflectionClass = new \ReflectionClass($className);
+            $className = '\\' . $reflectionClass->getName();
+        } catch (\Exception | \Throwable) {
+            // It's a scalar type, array or non-object
+        }
+
+        return $className;
+    }
+
+    /**
+     * @param array $parameters
+     * @return array
+     */
+    private function resolveParameters(array $parameters): array
+    {
+        $parameterResolved = [];
+        foreach ($parameters as $parameter) {
+            $paramName = $parameter->getName();
+            $parameterType = '';
+            if ($parameter->getType() instanceof \ReflectionNamedType) {
+                $parameterType = $parameter->getType()->getName();
+            } elseif ($parameter->getType() instanceof \ReflectionUnionType) {
+                $types = $parameter->getType()->getTypes();
+                $typeNameArray = [];
+                foreach ($types as $type) {
+                    $typeNameArray[] = $type->getName();
+                }
+                $parameterType = implode('|', $typeNameArray);
+            } elseif ($parameter->getType() instanceof \ReflectionIntersectionType) {
+                $types = $parameter->getType()->getTypes();
+                $typeNameArray = [];
+                foreach ($types as $typeElement) {
+                    $typeNameArray[] = $typeElement->getName();
+                }
+                $parameterType = implode('&', $typeNameArray);
+            }
+            // Check if the parameter allows null
+            $nullable = $parameter->allowsNull() ? '?' : '';
+            $optional = $parameter->isOptional();
+            $default = '';
+            if ($optional) {
+                try {
+                    $default = ArrayHelper::varExportArrayShort($parameter->getDefaultValue());
+                } catch (\ReflectionException) {
+                }
+            }
+            // Check if the parameter is passed by reference
+            $passedByReference = $parameter->isPassedByReference() ? '&' : '';
+
+            // Check if the parameter is variadic
+            $variadic = $parameter->isVariadic() ? '...' : '';
+
+            $parameterResolved[] = [
+                'name' => '$' . $paramName,
+                'type' => $parameterType,
+                'optional' => $optional,
+                'default' => $default,
+                'description' => '',
+                'passedByReference' => $passedByReference,
+                'variadic' => $variadic,
+                'nullable' => $nullable,
+            ];
+        }
+        return $parameterResolved;
+    }
+
+    private function enrichParametersResolvedWithDocComment(array $paramArray, array $parameterResolved): array
+    {
+        if (is_array($paramArray) && count($paramArray) > 0) {
+            foreach ($paramArray as $param) {
+                $paramCommentExplode = explode(' ', $param->render(), 4);
+                $paramName = null;
+                $parameterType = null;
+                $description = '';
+                if (count($paramCommentExplode) > 2) {
+                    $paramName = $paramCommentExplode[2];
+                    $parameterType = $paramCommentExplode[1];
+                    $description = $paramCommentExplode[3] ?? '';
+                }
+                foreach ($parameterResolved as $key => $paramResolved) {
+                    if ($parameterResolved[$key]['name'] === $paramName) {
+                        // Type from method reflection is considered more accurate
+                        if (!$parameterResolved[$key]['type']) {
+                            $parameterResolved[$key]['type'] = $parameterType;
+                        }
+                        $parameterResolved[$key]['description'] = $description;
+                    }
+                }
+            }
+        }
+        return $parameterResolved;
+    }
+
+    private function isInternal(\ReflectionMethod $methodReflection): bool
+    {
+        // For some reason $methodReflection->isInternal() is always false
+        return is_string($methodReflection->getDocComment())
+            && str_contains($methodReflection->getDocComment(), '* @internal');
+    }
+
+    /**
+     * @param bool $allowInternal
+     * @param bool $isInternal
+     * @param bool $allowDeprecated
+     * @param \ReflectionMethod $methodReflection
+     * @param int $modifierSum
+     * @param bool $includeConstructor
+     * @param string $method
+     * @return bool
+     */
+    public function isMethodDisAllowed(bool $allowInternal, bool $isInternal, bool $allowDeprecated, \ReflectionMethod $methodReflection, int $modifierSum, bool $includeConstructor, string $method): bool
+    {
+        return (!$allowInternal && $isInternal)
+            or (!$allowDeprecated && $methodReflection->isDeprecated())
+            or (($modifierSum & $methodReflection->getModifiers()) == 0)
+            or (!$includeConstructor && $method == '__construct');
+    }
+
+    private function findLineStart(false|int $startLineBody, \SplFileObject $splFileObject, string $method): int
+    {
+        $startLineSignature = max($startLineBody - 20, 0);
+        for ($lineNumber = $startLineSignature; $lineNumber <= $startLineBody; $lineNumber++) {
+            $splFileObject->seek($lineNumber);
+            if (str_contains($splFileObject->current(), sprintf('function %s', RstHelper::escapeRst($method)))) {
+                $startLineSignature = $lineNumber;
+            }
+        }
+        return $startLineSignature;
+    }
+
+    private function getParameters(array $parameterResolved): array
+    {
         $parameterInSignature = [];
 
         $parameters = [];
@@ -211,70 +307,25 @@ class MethodFactory
                 $modifiers,
             );
         }
-        $codeResult = [];
-        for ($lineNumber = $startLineSignature; $lineNumber < $endLineBody; $lineNumber++) {
-            $splFileObject->seek($lineNumber);
-            $codeResult[] = $splFileObject->current();
-        }
-        $code = implode('', $codeResult);
+        return [$parameterInSignature, $parameters];
+    }
 
-        $typeNames = '';
-        if ($returnType instanceof \ReflectionUnionType or $returnType instanceof \ReflectionNamedType && $returnType->getName() != 'void') {
-            if ($returnType instanceof \ReflectionNamedType) {
-                $typeNames = $returnType->allowsNull()
-                    ? '?' . self::getFullQualifiedClassNameIfPossible($returnType->getName())
-                    : self::getFullQualifiedClassNameIfPossible($returnType->getName());
-            } elseif ($returnType instanceof \ReflectionUnionType) {
-                $types = $returnType->getTypes();
-                $typeNameArray = [];
-                foreach ($types as $returnType) {
-                    $typeNameArray[] = $returnType->allowsNull()
-                        ? '?' . self::getFullQualifiedClassNameIfPossible($returnType->getName())
-                        : self::getFullQualifiedClassNameIfPossible($returnType->getName());
-                }
-                $typeNames = implode('|', $typeNameArray);
-            }
-        }
-
-        $modifiers = [];
+    private function extractTypeFromReturnComment(string $typeNames, array|string $returnComment): array
+    {
         $returnType = null;
         if ($typeNames !== '') {
             $returnType = new Type($typeNames);
         }
 
-        if (str_starts_with($returnComment, $typeNames . ' ')) {
-            $returnComment = str_replace($typeNames . ' ', '', $returnComment);
-            $returnComment = ucfirst(trim($returnComment));
-        } elseif ($returnComment !== '') {
-            // It is probably a complex type declaration
-            $returnType = new Type($returnComment);
+        $returnComment = trim($returnComment);
+        if ($returnComment !== '') {
+            $parts = explode(' ', $returnComment, 2);
             $returnComment = '';
+            $returnType = new Type($parts[0]);
+            if (isset($parts[1])) {
+                $returnComment = ucfirst(trim($parts[1]));
+            }
         }
-
-        // SplFileObject locks the file, so null it when no longer needed
-        $splFileObject = null;
-        //return $methodHead . $methodBody;
-        return new MethodMember(
-            $methodReflection->getName(),
-            $comment,
-            $modifiers,
-            $code,
-            $parameters,
-            $returnType,
-            str_replace("\n", '', $returnComment),
-            implode(', ', $parameterInSignature),
-        );
-    }
-
-    private function getFullQualifiedClassNameIfPossible(string $className): string
-    {
-        try {
-            $reflectionClass = new \ReflectionClass($className);
-            $className = '\\' . $reflectionClass->getName();
-        } catch (\Exception | \Throwable) {
-            // It's a scalar type, array or non-object
-        }
-
-        return $className;
+        return [$returnType, $returnComment];
     }
 }
