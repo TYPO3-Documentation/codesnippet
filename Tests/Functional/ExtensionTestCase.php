e692fb59c31d274084d60e456e754075bfc30348
lina.wolf <lwolf@w-commerce.de>
2024-06-01 06:33:25 +0200

    [TASK] Introduce test extension

--- Tests/Functional/ExtensionTestCase.php
+++ Tests/Functional/ExtensionTestCase.php
@@ -17,19 +17,6 @@ declare(strict_types=1);
 
 namespace T3docs\Codesnippet\Tests\Functional;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
@@ -53,6 +40,7 @@ abstract class ExtensionTestCase extends FunctionalTestCase
         $this->testExtensionsToLoad = [
             ...array_values($this->testExtensionsToLoad),
             't3docs/codesnippet',
+            'typo3conf/ext/codesnippet/Functional/Fixtures/Extensions/example_extension',
         ];
     }
 }
